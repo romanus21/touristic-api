dfe07edf8baf769a5793b0215f0ce3d43ad4234c
Роман Иванов <72571751+romanus21@users.noreply.github.com>
2023-05-14 16:58:51 +0300

    [bugFix]

--- app/tests.py
+++ app/tests.py
@@ -1,47 +1,14 @@
-import itertools
+from datetime import datetime
 
-from app.config import settings
-from app.db import get_engine
-from app.guide.cuckoo.cuckoo import CuckooAlgo
-from app.guide.dijkstra.route import get_djikstra_route
 from app.guide.cuckoo.route import (
-    get_graph,
-    get_sights,
-    get_node_by_coord,
+    get_cycle_route,
 )
-from app.utils.geometry import bbox_from_point
+from app.guide.dijkstra.route import get_djikstra_route
 
 
 def calc():
-    start_lat, start_lon = (59.90502001713264, 30.314780012851003)
-    desired_sights = 10
-
-    engine = get_engine()
-    start_point = (start_lat, start_lon)
-    bbox = bbox_from_point(start_point)
-
-    graph = get_graph(bbox, engine=engine)
-    sights = get_sights(bbox, graph, engine=engine)
-
-    start_node = get_node_by_coord(
-        lat=start_lat, lon=start_lon, bbox=bbox, engine=engine
-    )
-
-    max_fitness = 0
-    solution = []
-    for perm in itertools.permutations(sights.values()):
-        fitness = CuckooAlgo(
-            desired_sights=desired_sights,
-            sights=sights,
-            graph=graph,
-            start_node=start_node,
-            route_time=1,
-        ).fitness_calculation(list(perm))
-        if fitness > max_fitness:
-            max_fitness = fitness
-            solution = perm
-
-    print(solution)
+    start_lat, start_lon = (59.93512000000004, 30.311480000000074)
+    get_cycle_route(start_lat, start_lon, 60)
 
 
 def calc_route():
@@ -51,5 +18,17 @@ def calc_route():
 
 
 if __name__ == "__main__":
-    print(settings.BASE_DIR)
-    # calc_route()
+    # print(settings.BASE_DIR)
+    print('qwe')
+    start = datetime.now()
+    calc()
+    end = datetime.now()
+    print((end-start).seconds)
+    # start = datetime.now()
+    # calc()
+    # end = datetime.now()
+    # print((end-start).seconds)
+    # start = datetime.now()
+    # calc()
+    # end = datetime.now()
+    # print((end-start).seconds)
